e5db4cd32284b9f8a84d64d673237c9b232d119b
ssori <115159126+ssori0421@users.noreply.github.com>
2023-07-06 17:37:48 +0900

    style: event handler 이름 수정

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const BackDrop = ({ onClickToggleCart }) => {
+const BackDrop = ({ toggleCart }) => {
     return (
         <div
             id="backdrop"
             className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-            onClick={onClickToggleCart}
+            onClick={toggleCart}
         ></div>
     );
 };
